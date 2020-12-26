9da53daf9389af10d094df2f2353735d620c10dc
tada <tada3@outlook.com>
2020-12-26 15:47:30 +0900

    fixed a*a issue

--- problems/go/0010_regular-expression-matching/main.go
+++ problems/go/0010_regular-expression-matching/main.go
@@ -32,7 +32,13 @@ type token struct {
 
 type matcher struct {
 	tokens []token
-	pos    int
+	states []*state
+}
+
+type state struct {
+	isActive bool
+	pos      int
+	m        *matcher
 }
 
 func createMatcher(p string) *matcher {
@@ -50,76 +56,118 @@ func createMatcher(p string) *matcher {
 			})
 		}
 	}
-	return &matcher{
+	m := &matcher{
 		tokens: tokens,
-		pos:    0,
 	}
+	states := []*state{
+		{
+			isActive: true,
+			pos:      0,
+			m:        m,
+		}}
+	m.states = states
+	return m
 }
 
-func (m *matcher) match(c rune) bool {
-	fmt.Printf("MMMM %c, %d\n", c, m.pos)
+func (m *matcher) matchState(c rune, s *state) (bool, []*state) {
+	//fmt.Printf("MMMM %c, %d\n", c, m.pos)
 	if c == 0 {
-		if m.finished() {
-			return true
+		if s.finished() {
+			return true, nil
 		}
-		current := m.currentToken()
-		if m.isLastToken() && current.repeat {
-			return true
+		current := s.currentToken()
+		if s.atLastToken() && current.repeat {
+			return true, nil
 		}
-		return false
+		s.isActive = false
+		return false, nil
 	}
 
-	if m.finished() {
-		return false
+	if s.finished() {
+		s.isActive = false
+		return false, nil
 	}
 
-	current := m.currentToken()
+	current := s.currentToken()
 	if current.repeat {
 		if current.char == '.' {
 			// longest match, repeat forever
 			// do not check the next token
-			return true
+			return true, nil
 		} else {
 			if current.char == c {
-				return true
+				// 2 possible paths:
+				//    (a) stay this token
+				//    (b) move to next token
+				newStates := make([]*state, 0)
+				newState := &state{
+					isActive: true,
+					pos:      s.pos + 1,
+					m:        m,
+				}
+				_, newStates1 := m.matchState(c, newState)
+				newStates = append(newStates, newState)
+				newStates = append(newStates, newStates1...)
+				return true, newStates
 
 			} else {
 				// move to the next token
-				m.next()
-				return m.match(c)
+				s.next()
+				return m.matchState(c, s)
 			}
 		}
 
 	} else {
 		if current.char == '.' {
-			m.next()
-			return true
+			s.next()
+			return true, nil
 		} else {
 			if current.char == c {
-				m.next()
-				return true
+				s.next()
+				return true, nil
 
 			} else {
-				return false
+				return false, nil
 			}
 		}
 	}
 }
 
-func (m *matcher) finished() bool {
-	return m.pos == len(m.tokens)
+func (m *matcher) match(c rune) bool {
+	ret := false
+	added := make([]*state, 0)
+	for _, s := range m.states {
+		if !s.isActive {
+			continue
+		}
+		ok, newStates := m.matchState(c, s)
+		if ok {
+			ret = true
+		} else {
+			s.isActive = false
+		}
+		if newStates != nil {
+			added = append(added, newStates...)
+		}
+	}
+	m.states = append(m.states, added...)
+	return ret
 }
 
-func (m *matcher) isLastToken() bool {
-	return m.pos == len(m.tokens)-1
+func (s *state) finished() bool {
+	return s.pos == len(s.m.tokens)
 }
 
-func (m *matcher) next() {
-	m.pos++
+func (s *state) currentToken() token {
+	return s.m.tokens[s.pos]
 }
 
-func (m *matcher) currentToken() token {
-	return m.tokens[m.pos]
+func (s *state) atLastToken() bool {
+	return s.pos == len(s.m.tokens)-1
+}
+
+func (s *state) next() {
+	s.pos++
 }
 
 const filename = "testcase.txt"
