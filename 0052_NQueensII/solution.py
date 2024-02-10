7c55b3e3d1123d1d00460bd889de020416927ad9
yoshihara <tadafumi.yoshihara@linecorp.com>
2024-02-10 22:05:29 +0900

    Added 52

---
+++ 0052_NQueensII/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        return 0
+    
+
+
+
+
+def main(digits):
+    print(f'Input: digitis = {digits}')
+    ret = Solution().plusOne(digits)
+    print(f'Output: {ret}')
+
+
+
+
+if __name__ == "__main__":
+    digits = [1,2,3]
+    #digits = [4,3,2,1]
+    #digits = [9]
+   
+    main(digits)
